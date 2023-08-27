3637705e0e18e3b397f2800624d37e6a7f1f98a1
siyadas878 <siyadas878@gmail.com>
2023-08-27 11:27:45 +0530

    message sending and recieving completed

--- lib/application/message_provider/message_provider.dart
+++ lib/application/message_provider/message_provider.dart
@@ -1,35 +1,84 @@
+import 'dart:async';
 import 'dart:developer';
-
-import 'package:chat_app/domain/message_model/message_model.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import '../../domain/message_model/message_model.dart';
 
-class MessageCreationProvider extends ChangeNotifier{
-
+class MessageCreationProvider extends ChangeNotifier {
   final TextEditingController messageController = TextEditingController();
+  String userId = FirebaseAuth.instance.currentUser!.uid;
 
   Future<void> addMessage(String fromId) async {
-     
-     String userId=FirebaseAuth.instance.currentUser!.uid;
-     String messageId= DateTime.now().millisecondsSinceEpoch.toString();
-
+    String addinguid = fromId + userId;
+    List<String> charList = addinguid.split('');
+    charList.sort();
+    String uniqueId = charList.join();
+    
     try {
       MessageModel message = MessageModel(
         fromId: fromId,
         message: messageController.text,
-        messageId: messageId,
+        messageId: uniqueId,
         time: DateTime.now().toString(),
-        userId: userId);
+        userId: userId,
+      );
 
       CollectionReference usersCollection =
           FirebaseFirestore.instance.collection('chat');
 
       Map<String, dynamic> messageData = message.toJson();
 
-      await usersCollection.doc(userId).collection(userId+fromId).doc(messageId).set(messageData);
+      await usersCollection.doc(uniqueId).set({'id': uniqueId, 'fromId': fromId, 'userId': userId});
+
+      await usersCollection
+          .doc(uniqueId)
+          .collection('messages')
+          .add(messageData); 
+      
+      Timer(const Duration(seconds: 1), () {
+        messageController.clear();
+        notifyListeners();
+      });
     } catch (error) {
-      log("Error adding user: $error");
+      log("Error adding message: $error");
     }
+  }
+
+  Future<List<MessageModel>> getAllMessages(String fromId) async {
+  List<MessageModel> allmessages = [];
+  String addinguid = fromId + userId;
+  List<String> charList = addinguid.split('');
+  charList.sort();
+  String uniqueId = charList.join();
+
+  try {
+    var userCollectionSnapshot = await FirebaseFirestore.instance
+        .collection('chat')
+        .doc(uniqueId)
+        .collection('messages')
+        .orderBy('time', descending: false)
+        .get();
+
+    List<MessageModel> message = userCollectionSnapshot.docs.map((doc) {
+      Map<String, dynamic> data = doc.data();
+      return MessageModel.fromJson(data);
+    }).toList();
+
+    allmessages = message;
+    notifyListeners();
+    return allmessages;
+  } catch (e) {
+    log('Error getting messages: $e');
+  }
+
+  return [];
+}
+
+
+  @override
+  void dispose() {
+    messageController.dispose();
+    super.dispose();
   }
 }
--- lib/presentation/screens/message_screen/message_screen.dart
+++ lib/presentation/screens/message_screen/message_screen.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:provider/provider.dart';
-
+import 'package:firebase_auth/firebase_auth.dart';
 import '../../../application/message_provider/message_provider.dart';
+import '../../../domain/message_model/message_model.dart';
 
 class ChatScreen extends StatelessWidget {
   final String fromId;
-  const ChatScreen({Key? key,required this.fromId}) : super(key: key);
+
+  const ChatScreen({Key? key, required this.fromId}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
+    String userId = FirebaseAuth.instance.currentUser!.uid;
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.teal,
@@ -20,27 +23,48 @@ class ChatScreen extends StatelessWidget {
           children: [
             SizedBox(height: size.height * 0.02),
             Expanded(
-              child: ListView.separated(
-                padding:const EdgeInsets.symmetric(horizontal: 10),
-                separatorBuilder: (context, index) =>
-                    SizedBox(height: size.height * 0.02),
-                itemCount: 5,
-                itemBuilder: (context, index) {
-                  return Align(
-                    alignment: index % 2 == 0
-                        ? Alignment.centerLeft
-                        : Alignment.centerRight,
-                    child: Container(
-                      padding:const EdgeInsets.all(12),
-                      decoration: BoxDecoration(
-                        color: index % 2 == 0 ? Colors.teal : Colors.blue,
-                        borderRadius: BorderRadius.circular(20),
-                      ),
-                      child: Text(
-                        'This is a sample message $index',
-                        style:const TextStyle(color: Colors.white),
-                      ),
-                    ),
+              child: Consumer<MessageCreationProvider>(
+                builder: (context, messageProvider, _) {
+                  return FutureBuilder<List<MessageModel>>(
+                    future: messageProvider.getAllMessages(fromId),
+                    builder: (context, snapshot) {
+                      if (snapshot.hasError) {
+                        return Text('Error: ${snapshot.error}');
+                      } else if (!snapshot.hasData ||
+                          snapshot.data == null ||
+                          snapshot.data!.isEmpty) {
+                        return const Center(child: Text('No data available'));
+                      }
+
+                      final messages = snapshot.data!;
+
+                      return ListView.separated(
+                        padding: const EdgeInsets.symmetric(horizontal: 10),
+                        separatorBuilder: (context, index) =>
+                            SizedBox(height: size.height * 0.02),
+                        itemCount: messages.length,
+                        itemBuilder: (context, index) {
+                          return Align(
+                            alignment: messages[index].fromId == userId
+                                ? Alignment.centerLeft
+                                : Alignment.centerRight,
+                            child: Container(
+                              padding: const EdgeInsets.all(12),
+                              decoration: BoxDecoration(
+                                color: messages[index].fromId == userId
+                                    ? Colors.teal
+                                    : Colors.teal[300],
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                              child: Text(
+                                messages[index].message!,
+                                style: const TextStyle(color: Colors.white),
+                              ),
+                            ),
+                          );
+                        },
+                      );
+                    },
                   );
                 },
               ),
@@ -48,22 +72,23 @@ class ChatScreen extends StatelessWidget {
             Padding(
               padding: const EdgeInsets.all(8.0),
               child: Container(
-                height: size.height*0.06,
+                height: size.height * 0.06,
                 color: Colors.white,
                 child: Row(
                   children: [
                     Expanded(
                       child: TextField(
-                        controller: context.read<MessageCreationProvider>().messageController,
+                        controller: context
+                            .read<MessageCreationProvider>()
+                            .messageController,
                         maxLines: 1,
                         decoration: InputDecoration(
                           fillColor: Colors.white,
-                          prefixIcon: const Icon(FontAwesomeIcons.penToSquare),
                           hintText: 'Write your message...',
                           hintStyle: TextStyle(
                             color: Colors.black.withOpacity(0.3),
                           ),
-                          border:const OutlineInputBorder(
+                          border: const OutlineInputBorder(
                             borderRadius: BorderRadius.all(Radius.circular(30)),
                           ),
                         ),
@@ -72,16 +97,18 @@ class ChatScreen extends StatelessWidget {
                     const SizedBox(width: 10),
                     InkWell(
                       onTap: () {
-                        Provider.of<MessageCreationProvider>(context,listen: false).addMessage(fromId);
+                        context
+                            .read<MessageCreationProvider>()
+                            .addMessage(fromId);
                       },
                       child: Container(
-                        padding:const EdgeInsets.all(12),
-                        decoration:const BoxDecoration(
+                        padding: const EdgeInsets.all(12),
+                        decoration: const BoxDecoration(
                           color: Colors.teal,
                           shape: BoxShape.circle,
                         ),
-                        child:const Icon(
-                          FontAwesomeIcons.paperPlane,
+                        child: const Icon(
+                          Icons.send,
                           color: Colors.white,
                         ),
                       ),
@@ -96,4 +123,3 @@ class ChatScreen extends StatelessWidget {
     );
   }
 }
-
